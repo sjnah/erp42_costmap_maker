4c0a0e0c9c34ac7934100f9b11c4e6d29db85cdf
sjnah <vmflcm42@naver.com>
2021-11-23 00:20:27 +0900

    change getCost() and fix some bugs

--- scripts/erp42_costmap_maker.py
+++ scripts/erp42_costmap_maker.py
@@ -3,6 +3,7 @@ import os
 
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 
 import shapely.geometry as shp
 import yaml
@@ -10,12 +11,14 @@ import utm
 
 # file name
 filepath = './path/'
-# filename1 = 'FMTC_official_L.txt' # delimiter " " / offset ? m / gps_origin 37.36567790, 126.72499770
+# filename1 = 'FMTC_official_L.txt' # delimiter " " / offset ? m / gps_origin 37.36567790, 126.72499770 (정지선앞)
 # filename2 = 'FMTC_official_R.txt'
-# filename1 = 'FMTC_centerline_GP.txt' # delimiter "," / offset 2.5 m / gps_origin 37.36578294, 126.72539592
+# filename1 = 'FMTC_centerline_GP.txt' # delimiter "," / offset 2.5 m / gps_origin 37.36578294, 126.72539592 (차고앞)
 # filename2 = 'FMTC_centerline_GP.txt'
-filename1 = 'FMTC_simulator_1.txt' # delimiter "\t" / offset 1.25 m / gps_origin ???
-filename2 = 'FMTC_simulator_2.txt'
+# filename1 = 'FMTC_simulator_1.txt' # delimiter "\t" / offset 1.25 m / gps_origin ??? (중앙길 어딘가)
+# filename2 = 'FMTC_simulator_2.txt'
+filename1 = 'yonsei_3.txt'
+filename2 = 'yonsei_3.txt'
 
 # remap with new map origin
 remap = False
@@ -23,10 +26,11 @@ origin_old = [37.36567790, 126.72499770] # LL coordinate
 origin_new = [37.36578294, 126.72539592] # LL coordinate
 
 # Costmap configuration
-output_name = 'map_0_1_stopline_'
+output_name = 'map_yonsei_0_3'
 map_resolution = 0.1 # meter
 offset_length = 1.25 # meter
-boundary_size = 15 # meter
+boundary_size = 10 # meter
+wallsize = 0.4 # meter
 
 class CostmapMaker():
     def __init__(self):
@@ -47,25 +51,25 @@ class CostmapMaker():
 
         return waypoint_lane1, waypoint_lane2
 
-    def remap(self, poly_outward_before, poly_inward_before):
+    def remap(self):
         # Change Global Coordinate System (LL2UTM)
         X_old, Y_old, zone, unknown = utm.from_latlon(origin_old[0], origin_old[1])
         X_new, Y_new, zone, unknown = utm.from_latlon(origin_new[0], origin_new[1])
 
         # Transformation
-        poly_inward_after = poly_inward_before + [X_old - X_new, Y_old - Y_new]
-        poly_outward_after = poly_outward_before + [X_old - X_new, Y_old - Y_new]
+        self.poly_inward_1st = self.poly_inward_1st + [X_old - X_new, Y_old - Y_new]
+        self.poly_inward_2nd = self.poly_inward_2nd + [X_old - X_new, Y_old - Y_new]
+        self.poly_outward_1st = self.poly_outward_1st + [X_old - X_new, Y_old - Y_new]
+        self.poly_outward_2nd = self.poly_outward_2nd + [X_old - X_new, Y_old - Y_new]
         self.waypoint_lane1 = self.waypoint_lane1 + [X_old - X_new, Y_old - Y_new]
         self.waypoint_lane2 = self.waypoint_lane2 + [X_old - X_new, Y_old - Y_new]
 
-        return poly_outward_after, poly_inward_after
-
-    def setMapsize(self, poly_outward):
+    def setMapsize(self):
         # Add Boundary to map
-        self.max_x = np.max(poly_outward[:,0]) + boundary_size
-        self.min_x = np.min(poly_outward[:,0]) - boundary_size
-        self.max_y = np.max(poly_outward[:,1]) + boundary_size
-        self.min_y = np.min(poly_outward[:,1]) - boundary_size
+        self.max_x = np.max(self.poly_outward_2nd[:,0]) + boundary_size
+        self.min_x = np.min(self.poly_outward_2nd[:,0]) - boundary_size
+        self.max_y = np.max(self.poly_outward_2nd[:,1]) + boundary_size
+        self.min_y = np.min(self.poly_outward_2nd[:,1]) - boundary_size
 
     def getOffset(self, waypoint_lane1_raw, waypoint_lane2_raw):
         # Create a Polygon from the nx2 array in `afpts`
@@ -73,18 +77,20 @@ class CostmapMaker():
         waypoint_lane2_shp = shp.Polygon(waypoint_lane2_raw)
 
         # Create offset airfoils, both inward and outward
-        poly_inward_shp = waypoint_lane1_shp.buffer(-offset_length)  # Inward offset
-        poly_outward_shp = waypoint_lane2_shp.buffer(offset_length)  # Outward offset
+        poly_inward_1st_shp = waypoint_lane1_shp.buffer(-offset_length)  # Inward offset
+        poly_inward_2nd_shp = waypoint_lane1_shp.buffer(-offset_length-wallsize)  # Inward offset
+        poly_outward_1st_shp = waypoint_lane2_shp.buffer(offset_length)  # Outward offset
+        poly_outward_2nd_shp = waypoint_lane2_shp.buffer(offset_length+wallsize)  # Outward offset
 
         # Turn polygon points into numpy arrays for plotting
         self.waypoint_lane1 = np.array(waypoint_lane1_shp.exterior)
         self.waypoint_lane2 = np.array(waypoint_lane2_shp.exterior)
-        poly_outward = np.array(poly_outward_shp.exterior)
-        poly_inward = np.array(poly_inward_shp.exterior)
+        self.poly_outward_1st = np.array(poly_outward_1st_shp.exterior)
+        self.poly_outward_2nd = np.array(poly_outward_2nd_shp.exterior)
+        self.poly_inward_1st = np.array(poly_inward_1st_shp.exterior)
+        self.poly_inward_2nd = np.array(poly_inward_2nd_shp.exterior)
 
-        return poly_outward, poly_inward
-
-    def getCostmap(self, poly_outward, poly_inward):
+    def getCostmap(self):
         # Get costmap height and width
         height_meter = round(self.max_y) - round(self.min_y)
         width_meter = round(self.max_x) - round(self.min_x)
@@ -102,18 +108,18 @@ class CostmapMaker():
         # Calculate Costmap
         for i in range(width):
             for j in range(height):
-                # # Get current cell coordinate
-                # cell_x = round(self.min_x) + i*map_resolution + 0.5*map_resolution
-                # cell_y = round(self.max_y) - j*map_resolution - 0.5*map_resolution
+                # Get current cell coordinate
+                cell_x = round(self.min_x) + i*map_resolution + 0.5*map_resolution
+                cell_y = round(self.max_y) - j*map_resolution - 0.5*map_resolution
 
-                # # Check if the cell is inside the track
-                # cost = self.getCost(cell_x, cell_y, poly_outward, poly_inward)
+                # Check if the cell is inside the track
+                cost = self.getCost(cell_x, cell_y)
 
-                # # Set cell occupancy value
-                # map[j, i] = cost
+                # Set cell occupancy value
+                map[j, i] = cost
 
                 # Debugging
-                map[j, i] = 0
+                # map[j, i] = 0
             
             if (i%10 == 0):
                 progress = int((i*j)/total*100)
@@ -122,16 +128,17 @@ class CostmapMaker():
         print(map)
 
         # Set map origin
-        origin_x = 0 - round(self.min_x)
-        origin_y = 0 - round(self.min_y)
+        origin_x = round(self.min_x) - 0
+        origin_y = round(self.min_y) - 0
         origin_yaw = 0
         map_origin = [origin_x, origin_y, origin_yaw]
         map_origin = str(map_origin) + ' please remove single quote'
 
         plt.scatter(round(self.min_x), round(self.min_y), s=20, color='blue')
         plt.scatter(0,0, s=20, color='red')
-        plt.plot(*poly_outward.T, color='black')
-        plt.plot(*poly_inward.T, color='black')
+        plt.scatter(cell_x, cell_y, color='green')
+        plt.plot(*self.poly_outward_1st.T, color='black')
+        plt.plot(*self.poly_inward_1st.T, color='black')
         plt.plot(*self.waypoint_lane1.T, color='blue')
         plt.plot(*self.waypoint_lane2.T, color='blue')
         plt.axis('equal')
@@ -140,7 +147,7 @@ class CostmapMaker():
 
         # Make map.yaml data
         yaml_data = dict(
-            image = 'USERPATH/' + output_name + '.pgm', # Path to the image file containing the occupancy data; can be absolute, or relative to the location of the YAML file
+            image = './' + output_name + '.pgm', # Path to the image file containing the occupancy data; can be absolute, or relative to the location of the YAML file
             resolution = map_resolution, # Resolution of the map, meters / pixel
             origin = map_origin, # The 2-D pose of the lower-left pixel in the map, as (x, y, yaw)
             occupied_thresh =  0.65, # (0~1) Pixels with occupancy probability greater than this threshold are considered completely occupied.
@@ -150,31 +157,38 @@ class CostmapMaker():
         
         return map, yaml_data
         
-    def getCost(self, x, y, poly_outward, poly_inward):
+    def getCost(self, x, y):
         # Build Point and Polgon
         point = shp.Point(x, y)
-        polygon_inside = shp.polygon.Polygon(poly_inward)
-        polygon_outside = shp.polygon.Polygon(poly_outward)
+        polygon_inside_1st = shp.polygon.Polygon(self.poly_inward_1st)
+        polygon_inside_2nd = shp.polygon.Polygon(self.poly_inward_2nd)
+        polygon_outside_1st = shp.polygon.Polygon(self.poly_outward_1st)
+        polygon_outside_2nd = shp.polygon.Polygon(self.poly_outward_2nd)
 
         # check if the cell is inside the track 
-        inside = polygon_inside.contains(point) # bool
-        outside = polygon_outside.contains(point) # bool
+        inside_1st = polygon_inside_1st.contains(point) # bool
+        inside_2nd = polygon_inside_2nd.contains(point) # bool
+        outside_1st = polygon_outside_1st.contains(point) # bool
+        outside_2nd = polygon_outside_2nd.contains(point) # bool
 
-        if inside and outside:
+        if not outside_2nd:
+            cost = 128
+        elif not outside_1st and outside_2nd: # tra
             cost = 254
-            return cost
-        elif ~inside and outside:
-            cost = 1
-            return cost
-        else: # ~inside and ~outside
+        elif not inside_1st and outside_1st: # track
+            cost = 0
+        elif not inside_2nd and inside_1st:
             cost = 254
-            return cost
+        else: # inside_2nd
+            cost = 128
+
+        return cost
 
     def saveCostmap(self, map, yaml_data):
         # save costmap as pgm file
-        plt.imsave('./costmap/' + output_name + '_temp.png', map)
         plt.imsave('./costmap/' + output_name + '.png', map)
-        os.rename('./costmap/' + output_name + '_temp.png', './costmap/' + output_name + '.pgm')
+        # cv2.imwrite('./costmap/' + output_name + '.png', map)
+        cv2.imwrite('./costmap/' + output_name + '.pgm', map)
 
         # save costmap yaml
         with open('./costmap/' + output_name + '.yaml', 'w') as outfile:
@@ -187,15 +201,15 @@ class CostmapMaker():
         waypoint_lane1, waypoint_lane2 = self.loadPath()
 
         # get offset path line
-        poly_outward, poly_inward = self.getOffset(waypoint_lane1, waypoint_lane2)
+        self.getOffset(waypoint_lane1, waypoint_lane2)
 
         # remap with 2 gps origin point
         if remap == True:
-            poly_outward, poly_inward = self.remap(poly_outward, poly_inward)
+            self.remap()
 
         # get costmap
-        self.setMapsize(poly_outward)
-        map, yaml_data = self.getCostmap(poly_outward, poly_inward)
+        self.setMapsize()
+        map, yaml_data = self.getCostmap()
         self.saveCostmap(map, yaml_data)
         
 if __name__=="__main__":
